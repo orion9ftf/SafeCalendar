8b6e35f28e7d29f7a1ed4b826393a1a97114518b
estebanramirez19 <esteban.ramirez19@inacapmail.cl>
2026-07-13 19:11:09 -0400

    cambios de ruta carpeta templates a carpeta raiz - cambios gitignore

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from apps.events.models import Event
+
+def home(request):
+    events = Event.objects.filter(
+        is_active=True
+    ).order_by("event_date")
+
+    return render(
+        request,
+        "dashboard/home.html",
+        {
+            "events": events
+        }
+    )
+
+
+def event_detail(request, event_id):
+    event = get_object_or_404(
+        Event,
+        id=event_id,
+        is_active=True
+    )
+
+    return render(
+        request,
+        "dashboard/event_detail.html",
+        {
+            "event": event
+        }
+    )
--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 admin.site.site_header = "Centro de Inteligencia Calendario Blindado"
 admin.site.site_title = "Calendario Blindado"
 admin.site.index_title = "Panel de Monitoreo y Predicción"
 
 urlpatterns = [
+    path("", include("apps.dashboard.urls")),
     path("centro-inteligencia/", admin.site.urls),
 ]
 
